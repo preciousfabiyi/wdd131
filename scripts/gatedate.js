5645574307ef63bdbd9ba73680fbd2c181a04d12
precioufabiyi <preciousfabiyi10@gmail.com>
2026-03-03 20:12:47 +0100

    aa

---
+++ scripts/gatedate.js
@@ -0,0 +1,7 @@
+// Get current year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Get last modified date
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
