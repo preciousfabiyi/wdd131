276e6e4c87d95285d4564b32ef9951c53d9b3fa7
precioufabiyi <preciousfabiyi10@gmail.com>
2026-03-27 22:42:23 +0100

    pp

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -7,49 +7,63 @@ const temples = [
     location: "Salt Lake City, Utah, USA",
     dedicated: "1893, April, 6",
     area: 253000,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-8452.jpg"
   },
   {
     templeName: "Laie Hawaii Temple",
     location: "Laie, Hawaii, USA",
     dedicated: "1919, November, 27",
     area: 42100,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/laie-hawaii-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/laie-hawaii-temple/laie-hawaii-temple-3829.jpg"
   },
   {
     templeName: "Cardston Alberta Temple",
     location: "Cardston, Alberta, Canada",
     dedicated: "1923, August, 26",
     area: 88962,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/cardston-alberta-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/cardston-alberta-temple/cardston-alberta-temple-61960.jpg"
   },
   {
     templeName: "Accra Ghana Temple",
     location: "Accra, Ghana",
     dedicated: "2004, January, 11",
     area: 17500,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-5154.jpg"
   },
   {
     templeName: "Aba Nigeria Temple",
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
     area: 11500,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5092.jpg"
   },
   {
     templeName: "Rome Italy Temple",
     location: "Rome, Italy",
     dedicated: "2019, March, 10",
     area: 41000,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-3545.jpg"
   },
   {
     templeName: "Paris France Temple",
     location: "Paris, France",
     dedicated: "2017, May, 21",
     area: 44000,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple.jpg"
+    imageUrl: "hhttps://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2054.jpg"
+  },
+  {
+    templeName: "Tokyo Japan Temple",
+    location: "Tokyo, Japan",
+    dedicated: "2020, December, 20",
+    area: 50000,
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/tokyo-japan-temple/tokyo-japan-temple-27491.jpg"
+  },
+  {
+    templeName: "Barcelona Spain Temple",
+    location: "Barcelona, Spain",
+    dedicated: "2023, March, 5",
+    area: 60000,
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/barcelona-spain-temple/barcelona-spainhttps://churchofjesuschristtemples.org/assets/img/temples/barcelona-spain-temple/barcelona-spain-temple-43015.jpg-temple-1234.jpghttps://churchofjesuschristtemples.org/assets/img/temples/barcelona-spain-temple/barcelona-spain-temple-43015.jpg"
   }
 ];
 
