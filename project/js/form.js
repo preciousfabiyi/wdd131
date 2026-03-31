8003c78698e7def5cccd2d8a4ecba01fc4b37470
precioufabiyi <preciousfabiyi10@gmail.com>
2026-03-31 10:52:57 +0100

    pp

--- project/js/form.js
+++ project/js/form.js
@@ -17,3 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
     select.appendChild(option);
   });
 });
+const year = new Date().getFullYear();
+
+document.querySelector("footer p").innerHTML =
+`© ${year} Precious Fabiyi • Nigeria`;
+
+document.getElementById("lastModified").textContent =
+"Last Modification: " + document.lastModified;
